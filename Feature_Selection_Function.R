2a4d109c8a8c90c3951e7acb0d135d7c8639e53e
Majormajor2 <jahaz707@gmail.com>
2017-09-10 23:04:47 +0200

    Feature Selection using Boruta+RF

--- Feature_Selection_Function.R
+++ Feature_Selection_Function.R
@@ -129,4 +129,52 @@ print(boruta.df)
 
 
 
+##############################
+#### Feature Selection########
+##############################
+#install.packages("Boruta")
+library(Boruta)
+library(randomForest)
+
+#load saved datasets (can be found on slack: apa)
+boruta.train = readRDS(file = "boruta.train.RDS")
+rfe.train = readRDS(file = "rfe.train.RDS")
+
+# RF Feature Importance
+plot(rfe.train, type=c("g", "o"), cex = 1.0, col = 1:11)
+
+# Boruta Feature Selection
+print(boruta.train)
+
+# Boruta Tentative
+plot(boruta.train, xlab = "", xaxt = "n")
+lz=lapply(1:ncol(boruta.train$ImpHistory),function(i)
+  boruta.train$ImpHistory[is.finite(boruta.train$ImpHistory[,i]),i])
+names(lz) = colnames(boruta.train$ImpHistory)
+Labels = sort(sapply(lz,median))
+axis(side = 1,las=2,labels = names(Labels),
+     at = 1:ncol(boruta.train$ImpHistory), cex.axis = 0.7)
+
+
+# Boruta Final
+final.boruta = TentativeRoughFix(boruta.train)
+print(final.boruta)
+
+getSelectedAttributes(final.boruta, withTentative = F)
+
+
+boruta.df = attStats(final.boruta)
+class(boruta.df)
+print(boruta.df)
+
+
+plot(final.boruta, xlab = "", xaxt = "n")
+lz=lapply(1:ncol(final.boruta$ImpHistory),function(i)
+  final.boruta$ImpHistory[is.finite(final.boruta$ImpHistory[,i]),i])
+names(lz) = colnames(final.boruta$ImpHistory)
+Labels = sort(sapply(lz,median))
+axis(side = 1,las=2,labels = names(Labels),
+     at = 1:ncol(final.boruta$ImpHistory), cex.axis = 0.7)
+
+
 
