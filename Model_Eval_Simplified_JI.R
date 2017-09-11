62bd268b60cdac5c20ac20e934100623a3f42f89
Jay-iyer <jaya1992@gmail.com>
2017-09-12 01:04:26 +0200

    Added error visualization

--- Model_Eval_Simplified_JI.R
+++ Model_Eval_Simplified_JI.R
@@ -36,7 +36,38 @@ results <- function(probabilities,actual,threshold,pos,neg,name){
     print(confmat)
     cmplot <- fourfoldplot(confmat$table, color = c("#0000FF","#FF0000"),
                                   conf.level = 0, margin = 1,main =paste0("Threshold value - ",name) )
-  }else{print("classification has only one level")}
+  }else{print("classification has only one level"); break}
+  
+  # Calculate rsse
+  revenue.actual <- test$price*(as.numeric(test$order)-1)
+  print(summary(revenue.actual))
+  
+  revenue.pred <- (as.numeric(pred)-1)*test$price
+  print(summary(revenue.pred))
+  
+  error <- user(revenue.actual,revenue.pred)
+  print(paste0("The RSSE from prediction using threshold value ", threshold))
+  print(error)
+  
+  # Visualization - Plotting the normal curve
+  x1 = revenue.actual
+  y1 = dnorm(revenue.actual, mean = mean(revenue.actual), sd = sd(revenue.actual))
+  
+  x2 = revenue.pred
+  y2 = dnorm(revenue.pred, mean = mean(revenue.pred), sd = sd(revenue.pred))
+  
+  cat("\n", "Plotting the distributions of the actual and predicted values - green actual,red predicted")
+  
+  dist.plot = plot(x1, y1, col = "green", xlim = range(c(x1, x2)),
+                   ylim = range(c(y1,y2)), xlab = "Revenue", ylab = "Density")
+  points(x2, y2, col = "red")
+  title(main = paste0("Threshold value : ",threshold))
+  legend("topright", legend = c("Actual", "Predicted"), fill = c("green", "red"))
+
+  dist.plot
+  
+  pred = as.vector(pred)
+  return(pred)
 }
 
 # Evaluation function 
@@ -58,12 +89,12 @@ evaluate = function(type,model,modelname,data,actual,pos,neg,threshold,metric) {
     
     # Function call to results function
     
-    pred.random <- results(probabilities,actual,0.5,pos,neg,"Random")
     pred.mean <- results(probabilities,actual,stats[3],pos,neg,"Mean")
     pred.median <- results(probabilities,actual,stats[4],pos,neg,"Median")
     pred.3quad <- results(probabilities,actual,stats[5],pos,neg,"Third Quadrant")
+    pred.random <- results(probabilities,actual,0.5,pos,neg,"Random")
     
-    if(threshold>0){
+    if(!is.null(threshold)){
       print(paste0("Using user defined threshold - ",threshold))
       pred.user <- results(probabilities,actual,threshold,pos,neg,"User Defined") 
     }
