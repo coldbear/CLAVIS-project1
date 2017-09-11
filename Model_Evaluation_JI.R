d6fe850dfc348f7068217a4182c46c2373859ab2
Jay-iyer <jaya1992@gmail.com>
2017-09-11 20:18:52 +0200

    Modified

--- Model_Evaluation_JI.R
+++ Model_Evaluation_JI.R
@@ -11,6 +11,7 @@
 # Set the working directory 
 if (!require("rstudioapi")) install.packages("rstudioapi"); library("rstudioapi")
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(caret)
 
 # Evaluation metric
 rmse = function(actual, pred) {
@@ -45,7 +46,8 @@ evaluate = function(model,modelname,data, actual) {
     print("Descriptive statistics for the prediction probabilities : ")
     print(stats)
     
-    pos 
+    pos<- "yes"
+    neg <- "no"
     
     threshold = readline(prompt = "Do you want to use a specific threshold for classification?
                          Type in the numerical threshold value if not type 'no'")
@@ -54,10 +56,10 @@ evaluate = function(model,modelname,data, actual) {
       print("You have chosen to use a default threshold")
       print("Classification using 0.5,mean of the probabilities as thresholds")
       
-      pred_random = ifelse(probabilities >= 0.5, pos, neg)
-      pred_mean = ifelse(probabilities >= stats[3], pos, neg)
-      pred_median = ifelse(probabilities>=stats[4],pos, neg)
-      pred_3quad = ifelse(probabilities>=stats[5],pos, neg)
+      pred_random = as.factor(ifelse(probabilities >= 0.5, pos, neg))
+      pred_mean = as.factor(ifelse(probabilities >= stats[3], pos, neg))
+      pred_median = as.factor(ifelse(probabilities>=stats[4],pos, neg))
+      pred_3quad = as.factor(ifelse(probabilities>=stats[5],pos, neg))
       
       # Display results
       print("Actual")
@@ -65,17 +67,58 @@ evaluate = function(model,modelname,data, actual) {
       
       print("Random Classification")
       print(table(pred_random))
+      if(levels(pred_random)=="yes"&levels(pred_random)=="no"){
+      print("Confusion Matrix - Random")
+      cm.random <- confusionMatrix(test$order,pred_random)
+      print(cm.random)
+      cmplot.random <- fourfoldplot(cm.random$table, color = c("#0000FF","#FF0000"),
+                   conf.level = 0, margin = 1, main = "Threshold value -Random")
+      }else{ print("Random classification has only one level")}
+
       
       print("Threshold - Mean")
       print(table(pred_mean))
       
+      print("Confusion Matrix - Mean")
+      cm.mean <- confusionMatrix(test$order,pred_mean)
+      print(cm.mean)
+      cmplot.mean <- fourfoldplot(cm.mean$table, color = c("#0000FF","#FF0000"),
+                                    conf.level = 0, margin = 1, main = "Threshold value -mean")
+      
+      print("Threshold - median")
+      print(table(pred_median))
+      
+      print("Confusion Matrix - median")
+      cm.median <- confusionMatrix(test$order,pred_median)
+      print(cm.median)
+      cmplot.median <- fourfoldplot(cm.median$table, color = c("#0000FF","#FF0000"),
+                                  conf.level = 0, margin = 1, main = "Threshold value -median")
+      
+      print("Threshold - 3quad")
+      print(table(pred_3quad))
+      
+      print("Confusion Matrix - 3quad")
+      cm.3quad <- confusionMatrix(test$order,pred_3quad)
+      print(cm.3quad)
+      cmplot.3quad <- fourfoldplot(cm.3quad$table, color = c("#0000FF","#FF0000"),
+                                  conf.level = 0, margin = 1, main = "Threshold value -3quad")
+      
     } else {
-      pred_user = ifelse(probabilities >= as.numeric(threshold), pos, neg)
+  
+      pred_user = as.factor(ifelse(probabilities >= as.numeric(threshold), pos, neg))
       
       # Display results
-      cat(paste0("\n", "Classification with user defined threshold", as.numeric(threshold)))
+      cat(paste0("\n", "Classification with user defined threshold","\t" ,as.numeric(threshold)))
+      print(table(pred_user))
+      print("Threshold - user")
       print(table(pred_user))
       
+      print("Confusion Matrix - user")
+      cm.user <- confusionMatrix(test$order,pred_user)
+      print(cm.user)
+      cmplot.user <- fourfoldplot(cm.user$table, color = c("#0000FF","#FF0000"),
+                                   conf.level = 0, margin = 1, main = "Threshold value -user")
+
     }
     
   } else {
@@ -147,6 +190,8 @@ evaluate = function(model,modelname,data, actual) {
 
 
 #Evaluate the predictions - (Add your responses in the console)
-lr.res = evaluate(lr.model,"Linear Regression",test,Predictions_test$actual)
+ndata <- readRDS("fold_1_b2.rds")
+lr.model <- lm(train$order~.,train)
+lr.res = evaluate(lr.model,"Linear Regression",test,test$order)
 
 
