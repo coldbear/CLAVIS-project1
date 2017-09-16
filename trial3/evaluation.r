2457d178fba93ccc0bc852f71cd0dbfc05eb2f11
coldbear <coldbear@users.noreply.github.com>
2017-09-16 23:38:25 +0200

    saturday night

--- trial3/evaluation.r
+++ trial3/evaluation.r
@@ -5,15 +5,16 @@
 #################################################################### 
 # Set the working directory 
 # Uncomment these lines to set the working directory to the current file's location
-if (!require("rstudioapi")) install.packages("rstudioapi"); library("rstudioapi")
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# if (!require("rstudioapi")) install.packages("rstudioapi"); library("rstudioapi")
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+#### Note : For the rsse decomposition to work the dataset needs to have the price variable
 
 
 # Evaluation metric
-user <- function(actual, pred) {
-  rsse = sqrt(sum((actual - pred)^2))
-  return(rsse)
+sse <- function(actual, pred) {
+  error = sum((actual - pred)^2)
+  return(error)
 }
 
 # Custom results function
@@ -29,12 +30,11 @@ results <- function(probabilities,actual,threshold,pos,neg,name){
   print("Confusion Matrix")
   
   if(length(levels(pred))>1){
-    confmat<- confusionMatrix(actual,pred)
+    confmat <- caret::confusionMatrix(actual,pred)
     print(confmat)
     cmplot <- fourfoldplot(confmat$table, color = c("#0000FF","#FF0000"),
                            conf.level = 0, margin = 1,main =paste0("Threshold value - ",name) )
-  }
-  #else{print("classification has only one level"); break}
+  }else{print("classification has only one level"); break}
   
   #Calculate rsse
   revenue.actual <- test$price*(as.numeric(test$order)-1)
@@ -43,43 +43,81 @@ results <- function(probabilities,actual,threshold,pos,neg,name){
   revenue.pred <- (as.numeric(pred)-1)*test$price
   print(summary(revenue.pred))
   
-  error <- user(revenue.actual,revenue.pred)
+  sse.full <- sse(revenue.actual,revenue.pred)
+  error <- sqrt(sse.full)
   print(paste0("The RSSE from prediction using threshold value ", threshold))
-  print(error)
+  cat("\n",error)
   
-  # Error decomposition
-  mse = mean((revenue.actual - revenue.pred)^2)
-  bias = (mean(revenue.pred) - mean(revenue.actual))
-  var = mse - (bias^2) #(Decomposition of mse)
-  percent.var = var/mse
-  actual.skew = skewness(revenue.actual)  # to check the distributions
-  predicted.skew = skewness(revenue.pred)
+  # RSSE decomposition
+  res = as.data.frame(cbind(revenue.actual,revenue.pred))
+  res.low <- subset(res,res$revenue.actual<4)
+  res.high <- subset(res,res$revenue.actual>=4)
   
-  # Saving results
+  # Low value item Decomposition
+  sse.low <- sse(res.low$revenue.actual,res.low$revenue.pred)
+  percent.sse.low <- sse.low/sse.full
+  cat("\n","The percentage of error for low value items is ",percent.sse.low)
   
-  error.matrix = cbind(error, mse, bias,var,percent.var,actual.skew, predicted.skew)
-  cat("\n", "Decomposing the error : ", "\n")
-  print(error.matrix)
+  bias.low = (mean(res.low$revenue.actual) - mean(res.low$revenue.pred))
+  actual.skew.low = skewness(res.low$revenue.actual)  # to check the distributions
+  predicted.skew.low = skewness(res.low$revenue.pred)
+  error.low = cbind(sse.low,bias.low,actual.skew.low, predicted.skew.low)
+  cat("\n", "Decomposing the error for low value items: ", "\n")
+  print(error.low)
   
-  # Visualization - Plotting the normal curve
-  x1 = revenue.actual
-  y1 = dnorm(revenue.actual, mean = mean(revenue.actual), sd = sd(revenue.actual))
+  # Visualization - Plotting the normal curve - low value 
+  x1.low = res.low$revenue.actual
+  y1.low = dnorm(res.low$revenue.actual, 
+                 mean = mean(res.low$revenue.actual), sd = sd(res.low$revenue.actual))
   
-  x2 = revenue.pred
-  y2 = dnorm(revenue.pred, mean = mean(revenue.pred), sd = sd(revenue.pred))
+  x2.low = res.low$revenue.pred
+  y2.low = dnorm(res.low$revenue.pred, 
+                 mean = mean(res.low$revenue.pred), sd = sd(res.low$revenue.pred))
   
-  cat("\n", "Plotting the distributions of the actual and predicted values - green actual,red predicted")
+  cat("\n", "Plotting the distributions of the actual and predicted values for low revenue items")
   
-  dist.plot = plot(x1, y1, col = "green", xlim = range(c(x1, x2)),
-                   ylim = range(c(y1,y2)), xlab = "Revenue", ylab = "Density")
-  points(x2, y2, col = "red")
-  title(main = paste0("Threshold value : ",threshold))
+  dist.plot.low = plot(x1.low, y1.low, col = "green", xlim = range(c(x1.low, x2.low)),
+                       ylim = range(c(y1.low,y2.low)), xlab = "Revenue", ylab = "Density")
+  points(x2.low, y2.low, col = "red")
+  title(main = paste0("Distribution of actual vs predicted for 
+                      low value item with threshold value : ",threshold))
   legend("topright", legend = c("Actual", "Predicted"), fill = c("green", "red"))
   
-  dist.plot
+  dist.plot.low
   
-  # pred = as.vector(pred)
-  # return(pred)
+  # High value item Decomposition
+  sse.high <- sse(res.high$revenue.actual,res.high$revenue.pred)
+  percent.sse.high <- sse.high/sse.full
+  cat("\n","The percentage of error for high value items is ",percent.sse.high)
+  
+  bias.high = (mean(res.high$revenue.actual) - mean(res.high$revenue.pred))
+  actual.skew.high = skewness(res.high$revenue.actual)  # to check the distributions
+  predicted.skew.high = skewness(res.high$revenue.pred)
+  error.high = cbind(sse.high,bias.high,actual.skew.high, predicted.skew.high)
+  cat("\n", "Decomposing the error for high value items: ", "\n")
+  print(error.high)
+  
+  # Visualization - Plotting the normal curve - high value 
+  x1.high = res.high$revenue.actual
+  y1.high = dnorm(res.high$revenue.actual, 
+                  mean = mean(res.high$revenue.actual), sd = sd(res.high$revenue.actual))
+  
+  x2.high = res.high$revenue.pred
+  y2.high = dnorm(res.high$revenue.pred, 
+                  mean = mean(res.high$revenue.pred), sd = sd(res.high$revenue.pred))
+  
+  cat("\n", "Plotting the distributions of the actual and predicted values for high revenue items")
+  
+  dist.plot.high = plot(x1.high, y1.high, col = "green", xlim = range(c(x1.high, x2.high)),
+                        ylim = range(c(y1.high,y2.high)), xlab = "Revenue", ylab = "Density")
+  points(x2.high, y2.high, col = "red")
+  title(main = paste0("Distribution of actual vs predicted for 
+                      high value item with threshold value : ",threshold))
+  legend("topright", legend = c("Actual", "Predicted"), fill = c("green", "red"))
+  
+  dist.plot.high
+  
+  
 }
 
 # Evaluation function 
@@ -119,11 +157,4 @@ evaluate = function(model,modelname,data,actual,pos,neg,threshold) {
     pred.user <- results(probabilities,actual,threshold,pos,neg,"User Defined") 
   }
 }
-
-
-rf.res = evaluate(model = rf.model,
-                  modelname = "Random Forest",
-                  data = test,actual = test$order,
-                  pos = 1,neg = 0,
-                  threshold="Mean")
 
