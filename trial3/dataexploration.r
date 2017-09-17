03865f97b16ca505c727899d7861b44437e8f29a
ste-phanie <finkenws@hu-berlin.de>
2017-09-17 11:10:19 +0200

    adds boxplots

--- trial3/dataexploration.r
+++ trial3/dataexploration.r
@@ -16,5 +16,22 @@ pairs(train[,c(1,3,4,5,8)],col=train$order)
 }
 #e <- scatter.smooth(train$price,train$order)
 
+## BOXPLOTS ##
+get_boxplots <- function(train){
+  
+  # select all numerical variables except certain ones
+  # which ones are numerical?
+  numericals<-names(sapply(train, is.numeric))
+  remove <- c("adFlag","availability","cPriceNA","unit","genericProduct","salesIndex","campaignIndex","X3.1.1.Unique_pids_per_group_binned.DB","X3.1.2.Unique_pids_per_group_binned_2.DB","X3.2.1.Unique_pids_per_manufacturer_binned.DB","X3.2.2.Unique_pids_per_manufacturer_binned_2.DB","X3.3.1.Unique_pids_per_category_binned.DB","X3.4.1.Unique_pids_per_day_binned.DB","day2")
+  tobetreated <-numericals [! numericals %in% remove]
+  tobetreated <- tobetreated[tobetreated %in% colnames(train)]
+  
+  # rearrange data (longform)
+  train.m <- melt(train[,tobetreated], id.vars = "order") # only take numerical values for boxplot
+  
+  # plot
+  p <- ggplot(data = train.m, aes(x=variable, y=value)) 
+  p + geom_boxplot(aes(fill="order")) + facet_wrap( ~ variable, scales="free") 
+}
 
 
