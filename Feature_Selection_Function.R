eeee018d6bf8c4c890118b4c8ddd2fd0853caf19
Majormajor2 <jahaz707@gmail.com>
2017-09-17 20:26:19 +0200

    DATASET

--- Feature_Selection_Function.R
+++ Feature_Selection_Function.R
@@ -25,7 +25,9 @@ fold_1$order = ifelse(fold_1$revenue > 0,1,0)
 
 #take a stratified sample 
 set.seed(222)
+#set.seed(211)
 idx = createDataPartition(y = fold_1$order, p = 0.034, list = FALSE)
+idx_test = createDataPartition(y = fold_1$order, p = 0.0025, list = FALSE)
 fold_1 = fold_1[idx,]
 
 #Remove variables not needed
@@ -86,10 +88,42 @@ traindata = cbind(x, fold_1$order)
 colnames(traindata)[52] = "order"
 rm(fold_1)
 rm(x)
+#test = cbind(x, fold_1$order)
+
+new_names = c("pid",	"price",	"content",	"group",	
+              "category",	"meanPrice_deviation",	"last_observation",	
+              "adFlag_prop_obs_5",	"adFlag_prop_obs_10",	
+              "adFlag_prop_obs_15",	"adFlag_prop_obs_20",	
+              "adFlag_prop_obs_30",	"adFlag_prop_obs_50",	
+              "adFlag_last_1_day",	"adFlag_last_0_day",	
+              "adFlag_last_1_obs",	"adFlag_last_0_obs",	
+              "adFlag_last_1_obs_norm",	"availability2_prop_obs_15",	
+              "availability3_prop_obs_75",	"availability3_prop_obs_100",	
+              "availability4_prop_obs_100",	"pid_prop_per_day",	
+              "Unique_pids_per_category",	"Unique_pids_per_day",	
+              "weekday",	"availability_changes12_obs_5",	
+              "availability_changes42_obs_5",	"availability_changes13_obs_10"
+              ,"availability_changes21_obs_10","availability_changes32_obs_20",	
+              "availability_changes42_obs_30",	"availability_changes23_obs_50",
+              "availability_changes24_obs_50",	"availability_changes31_obs_50",
+              "availability_changes32_obs_75",	"availability_changes14_obs_100",
+              "availability_changes21_obs_100",	"availability_changes34_obs_100",	
+              "cPriceNA",	"unit",	"genericProduct",	"salesIndex",	"campaignIndex",
+              "Unique_pids_per_group_binned",	"Unique_pids_per_group_binned_2",	
+              "Unique_pids_per_manufacturer_binned",	"Unique_pids_per_manufacturer_binned_2",	
+              "Unique_pids_per_category_binned","Unique_pids_per_day_binned",	
+              "day2", "order")
+
+
+colnames(traindata)[1:52] = new_names
+colnames(test)[1:52] = new_names
+
+saveRDS(object = traindata, file = "train.rds")
+
 
 
 set.seed(223)
-idx_2 = createDataPartition(y = traindata$order, p = 0.7, list = FALSE)
+idx_2 = createDataPartition(y = fold_1$order, p = 0.7, list = FALSE)
 train = traindata[idx_2,]
 test = traindata[-idx_2,]
 rf.model = readRDS(file = "rfe.train.RDS")
@@ -164,7 +198,7 @@ rfe.train = readRDS(file = "rfe.train.RDS")
 
 
 
-Select_Features = function(method = c("RF", "Boruta_Tentative","Boruta_Final")){ 
+Select_Features = function(method = c("Random Forest", "Boruta_Tentative","Boruta_Final")){ 
   require(Boruta)
   require(randomForest)
   
@@ -178,7 +212,29 @@ Select_Features = function(method = c("RF", "Boruta_Tentative","Boruta_Final")){
     plot(boruta.train,xlab = "", xaxt = "n")
     lz=lapply(1:ncol(boruta.train$ImpHistory),function(i)
       boruta.train$ImpHistory[is.finite(boruta.train$ImpHistory[,i]),i])
-    names(lz) = colnames(boruta.train$ImpHistory)
+    names(lz) = c("pid",	"price",	"content",	"group",	
+                  "category",	"meanPrice_deviation",	"last_observation",	
+                  "adFlag_prop_obs_5",	"adFlag_prop_obs_10",	
+                  "adFlag_prop_obs_15",	"adFlag_prop_obs_20",	
+                  "adFlag_prop_obs_30",	"adFlag_prop_obs_50",	
+                  "adFlag_last_1_day",	"adFlag_last_0_day",	
+                  "adFlag_last_1_obs",	"adFlag_last_0_obs",	
+                  "adFlag_last_1_obs_norm",	"availability2_prop_obs_15",	
+                  "availability3_prop_obs_75",	"availability3_prop_obs_100",	
+                  "availability4_prop_obs_100",	"pid_prop_per_day",	
+                  "Unique_pids_per_category",	"Unique_pids_per_day",	
+                  "weekday",	"availability_changes12_obs_5",	
+                  "availability_changes42_obs_5",	"availability_changes13_obs_10"
+                  ,"availability_changes21_obs_10","availability_changes32_obs_20",	
+                  "availability_changes42_obs_30",	"availability_changes23_obs_50",
+                  "availability_changes24_obs_50",	"availability_changes31_obs_50",
+                  "availability_changes32_obs_75",	"availability_changes14_obs_100",
+                  "availability_changes21_obs_100",	"availability_changes34_obs_100",	
+                  "cPriceNA",	"unit",	"genericProduct",	"salesIndex",	"campaignIndex",
+                  "Unique_pids_per_group_binned",	"Unique_pids_per_group_binned_2",	
+                  "Unique_pids_per_manufacturer_binned",	"Unique_pids_per_manufacturer_binned_2",	
+                  "Unique_pids_per_category_binned","Unique_pids_per_day_binned",	
+                  "day2",	"shadowMax",	"shadowMean",	"shadowMin")
     Labels = sort(sapply(lz,median))
     axis(side = 1,las=2,labels = names(Labels),
          at = 1:ncol(boruta.train$ImpHistory), cex.axis = 0.7)
@@ -190,7 +246,29 @@ Select_Features = function(method = c("RF", "Boruta_Tentative","Boruta_Final")){
     plot(final.boruta, xlab = "", xaxt = "n")
     lz=lapply(1:ncol(final.boruta$ImpHistory),function(i)
       final.boruta$ImpHistory[is.finite(final.boruta$ImpHistory[,i]),i])
-    names(lz) = colnames(final.boruta$ImpHistory)
+    names(lz) =     names(lz) = c("pid",	"price",	"content",	"group",	
+                                  "category",	"meanPrice_deviation",	"last_observation",	
+                                  "adFlag_prop_obs_5",	"adFlag_prop_obs_10",	
+                                  "adFlag_prop_obs_15",	"adFlag_prop_obs_20",	
+                                  "adFlag_prop_obs_30",	"adFlag_prop_obs_50",	
+                                  "adFlag_last_1_day",	"adFlag_last_0_day",	
+                                  "adFlag_last_1_obs",	"adFlag_last_0_obs",	
+                                  "adFlag_last_1_obs_norm",	"availability2_prop_obs_15",	
+                                  "availability3_prop_obs_75",	"availability3_prop_obs_100",	
+                                  "availability4_prop_obs_100",	"pid_prop_per_day",	
+                                  "Unique_pids_per_category",	"Unique_pids_per_day",	
+                                  "weekday",	"availability_changes12_obs_5",	
+                                  "availability_changes42_obs_5",	"availability_changes13_obs_10"
+                                  ,"availability_changes21_obs_10","availability_changes32_obs_20",	
+                                  "availability_changes42_obs_30",	"availability_changes23_obs_50",
+                                  "availability_changes24_obs_50",	"availability_changes31_obs_50",
+                                  "availability_changes32_obs_75",	"availability_changes14_obs_100",
+                                  "availability_changes21_obs_100",	"availability_changes34_obs_100",	
+                                  "cPriceNA",	"unit",	"genericProduct",	"salesIndex",	"campaignIndex",
+                                  "Unique_pids_per_group_binned",	"Unique_pids_per_group_binned_2",	
+                                  "Unique_pids_per_manufacturer_binned",	"Unique_pids_per_manufacturer_binned_2",	
+                                  "Unique_pids_per_category_binned","Unique_pids_per_day_binned",	
+                                  "day2",	"shadowMax",	"shadowMean",	"shadowMin")
     Labels = sort(sapply(lz,median))
     axis(side = 1,las=2,labels = names(Labels),
          at = 1:ncol(final.boruta$ImpHistory), cex.axis = 0.7)
@@ -206,7 +284,7 @@ Select_Features(method = "Boruta_Tentative")
 Select_Features(method = "Boruta_Final")
 
 
-#RF Feature Importance#
+#Random Forest Feature Importance#
 #The plot shows how the RMSE changes as we increase the number of variables,
 # the variables corresponding to the lowest RMSE are "selected" and can 
 # be found using the "predictors" function.
@@ -226,5 +304,9 @@ Select_Features(method = "Boruta_Final")
 # respectively rejected and confirmed attributes#
 
 ## SOURCE: (Kursa and Rudnicki, 2010)
+
+
+
+
 
 
